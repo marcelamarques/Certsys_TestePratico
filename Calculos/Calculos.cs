fb2a10f38327f87ef66c6366f23689cedb521cc7
Marcela Marques <marcelamarques.br@gmail.com>
2020-07-27 19:17:45 -0300

    Add project Services and Calculos

---
+++ Calculos/Calculos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    public class Calculos
+    {
+        private double CalcDistMax(double max, double total)
+        {
+            double qnt = Math.Ceiling(total / max);
+
+            return total / qnt;
+        }
+
+        private int CalcPilarReforco(double distPilar, double distReforco)
+        {
+            return (int) Math.Floor(distReforco / distPilar);
+        }
+
+        public double TestCalcDistMax(double one, double two)
+        {
+            return CalcDistMax(one, two);
+        }
+
+        public int TestCalcPilarReforco(double distone, double disttwo)
+        {
+            return CalcPilarReforco(distone, disttwo);
+        }
+    }
+}
---
+++ Certsys.Services/CalculatePilars.cs
@@ -0,0 +1,105 @@
+﻿using Certsys.Domain;
+using Repository.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certsys.Services
+{
+    public static class CalculatePilars
+    {
+        public static double CalculateDistPilars(double disMax, double distTotal)
+        {
+            Calculos.Calculos Calculos = new Calculos.Calculos();
+            return Calculos.TestCalcDistMax(disMax, distTotal);
+        }
+
+        public static double CalculateReinforcementPilar(double distpilar, double distmax)
+        {
+            Calculos.Calculos Calculos = new Calculos.Calculos();
+
+            return Calculos.TestCalcPilarReforco(distpilar, distmax) * distpilar;
+        }
+
+        public static List<Pilar> GetPilars(int number, double distane)
+        {
+            List<Pilar> pilars = new List<Pilar>();
+            pilars.Add( new Pilar { Position = 1, Reinforced = true});
+            for (int i = 0; i < number-1; i++)
+            {
+                pilars.Add(new Pilar { Distance = distane * (i + 1), Position = i + 2 });
+            }
+            Pilar last = pilars.LastOrDefault();
+            pilars.Add(new Pilar { Position = last.Position+1, Distance = last.Distance + distane , Reinforced = true });
+
+            return pilars;
+        }
+
+        public static List<Pilar> SetPilarReinforced(double distMax, List<Pilar> pilars)
+        {
+            List<Pilar> auxpilar = new List<Pilar>(pilars);
+            foreach (Pilar item in pilars)
+            {
+                if (!item.Reinforced && item.Distance % distMax == 0)
+                {
+                    Pilar aux = new Pilar
+                    {
+                        Distance = item.Distance,
+                        Position = item.Position,
+                        Reinforced = true
+                    };
+                    auxpilar.RemoveAt(item.Position - 1);
+                    auxpilar.Insert(item.Position - 1, aux);
+                }
+            }
+            return auxpilar;
+        }
+
+        public static List<Pilar> GetPilarsReinforced(List<Pilar> pilars)
+        {
+            return pilars.Where(w => w.Reinforced).ToList();
+        }
+
+        private static Configuration FactoryConfiguration(double dist_total, double dist_vao, double dist_reforco)
+        {
+            return new Configuration { 
+                DistanciaMaxVao = dist_vao,
+                DistanceTotal = dist_total,
+                DistanceReinforcement= 2.0,
+            };
+        }
+
+        public static Project Project(double dist_total, double dist_vao, double dist_reforco)
+        {
+            Configuration config = FactoryConfiguration(dist_total, dist_vao, dist_reforco);
+            return new Project(config);
+        }
+
+
+        public static Project CalculateProject(Project project)
+        {
+            double vao = CalculateDistPilars(project.Configuration.DistanciaMaxVao, project.Configuration.DistanceTotal);
+            double refor = CalculateReinforcementPilar(vao, project.Configuration.DistanceReinforcement);
+            double number = project.Configuration.DistanceTotal / vao;
+            List<Pilar> pilars = GetPilars((int)number, vao);
+            List<Pilar> pilarReforco = SetPilarReinforced(refor, pilars);
+            List<Pilar> onlyReinforced = GetPilarsReinforced(pilarReforco);
+
+            project.Pilars = pilarReforco;
+            return project;
+        }
+
+        public static void SaveProject(Project project)
+        {
+            using (ProjectRepository repository = new ProjectRepository())
+            {
+                repository.Add(project);
+                repository.Commit();
+            }
+        }
+    }
+}
